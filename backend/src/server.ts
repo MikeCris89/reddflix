f46601892b114b2c073b2c8a45116168daa381f4
MikeCris <149326586+MikeCris89@users.noreply.github.com>
2026-05-12 00:30:18 -0400

    feat(backend): basic express server with port config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,13 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.listen(PORT, () => {
+	console.log(`Server running on http://localhost:${PORT}`);
+});
 
 app.use(cors());
